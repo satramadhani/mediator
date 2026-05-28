e5b2917d77f8317d2176bda89246f733fb1f1b67
satramadhani <satramadhani@outlook.com>
2026-05-28 09:41:01 +0700

    feat: Add base implementation

---
+++ src/Mediator/Impl/Mediator.cs
@@ -0,0 +1,46 @@
+using System.Collections.Concurrent;
+using System.Reflection;
+
+namespace Mediator.Impl;
+
+internal class Mediator(IServiceProvider provider) : IMediator
+{
+    private static readonly ConcurrentDictionary<Type, Type> VoidHandlerTypeCache = new();
+    private static readonly ConcurrentDictionary<Type, Type> ResultHandlerTypeCache = new();
+    private static readonly ConcurrentDictionary<Type, MethodInfo> MethodCache = new();
+
+    public async Task SendAsync(IRequest request, CancellationToken cancellationToken = default)
+    {
+        var requestType = request.GetType();
+        var handlerType = VoidHandlerTypeCache.GetOrAdd(
+            requestType,
+            t => typeof(IRequestHandler<>).MakeGenericType(t));
+
+        var handler = provider.GetService(handlerType);
+        if (handler == null)
+        {
+            throw new InvalidOperationException($"No handler found for request of type {request.GetType().Name}.");
+        }
+
+        var method = MethodCache.GetOrAdd(handlerType, t => t.GetMethod(nameof(IRequestHandler<>.HandleAsync))!);
+        await (Task)method.Invoke(handler, [request, cancellationToken])!;
+    }
+
+    public async Task<TResult> SendAsync<TResult>(IRequest<TResult> request, CancellationToken cancellationToken = default)
+    {
+        var requestType = request.GetType();
+        var handlerType = ResultHandlerTypeCache.GetOrAdd(
+            requestType,
+            t => typeof(IRequestHandler<,>).MakeGenericType(t, typeof(TResult)));
+
+        var handler = provider.GetService(handlerType);
+        if (handler == null)
+        {
+            throw new InvalidOperationException($"No handler found for request of type {request.GetType().Name}.");
+        }
+
+        var method = MethodCache.GetOrAdd(handlerType, t => t.GetMethod(nameof(IRequestHandler<,>.HandleAsync))!);
+        return await (Task<TResult>)method.Invoke(handler, [request, cancellationToken])!;
+    }
+}
+
